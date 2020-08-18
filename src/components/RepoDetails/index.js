c563331b2bbb31a3e21081eb534831cabade7480
douhimed <mr.douhi.med@gmail.com>
2020-08-18 23:43:58 +0100

    first commit 3

--- src/components/RepoDetails/index.js
+++ src/components/RepoDetails/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { formatDate } from "../../utilities/dates";
 
-const RepoDetails = (props) => {
-  return <h4 className="ui header">Repo Details</h4>;
+const RepoDetails = ({ repo }) => {
+  return (
+    <div className="card" key={repo.id}>
+      <div className="image">
+        <img src={repo.owner.avatar_url} alt="Avatar" />
+      </div>
+      <div className="content">
+        <div className="header">{repo.owner.login}</div>
+        <div className="description">{repo.description}</div>
+      </div>
+      <div className="extra content">
+        <span className="right floated">
+          Joined in {formatDate(repo.created_at)}
+        </span>
+        <span>
+          <i className="star icon"></i>
+          {repo.stargazers_count}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default RepoDetails;
